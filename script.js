6c2326687c3d7f43b40c1d69c4eab01b01e22d8a
Rajsinh1921 <143887210+Rajsinh1921@users.noreply.github.com>
2023-10-29 02:00:56 +0530

    First_page_complited

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-// Script for navigation bar
+const slidingMenu = document.getElementById("slideMenu");
+const hamMenu = document.getElementById("hamMenu");
+const closebtn = document.getElementById("close");
+
+
+
+hamMenu.addEventListener("click", () => {
+    slidingMenu.classList.add("active");
+});
+
+closebtn.addEventListener("click", () => {
+    slidingMenu.classList.remove("active");
+});
